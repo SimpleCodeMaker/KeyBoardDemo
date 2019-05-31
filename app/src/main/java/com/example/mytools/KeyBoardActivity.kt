efef372b47ca682231705389f66ce03d45d17feb
casual <123127611@qq.com>
2019-05-31 18:14:33 +0800

    完美解决一切问题

--- app/src/main/java/com/example/mytools/KeyBoardActivity.kt
+++ app/src/main/java/com/example/mytools/KeyBoardActivity.kt
@@ -1,19 +1,20 @@
 package com.example.mytools
 
 import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.inputmethod.InputMethodManager
-import com.example.mytools.adapter.SimpleAdapter
-import kotlinx.android.synthetic.main.activity_keyboard.*
-import android.graphics.Rect
 import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.FrameLayout
+import com.example.mytools.adapter.SimpleAdapter
+import kotlinx.android.synthetic.main.activity_keyboard.*
 
 
 class KeyBoardActivity : AppCompatActivity() {
@@ -26,7 +27,7 @@ class KeyBoardActivity : AppCompatActivity() {
         val inputLayout = LayoutInflater.from(this).inflate(R.layout.layout_input, null)
         val headview = LayoutInflater.from(this).inflate(R.layout.item_foot, recycler.parent as ViewGroup,false)
 //        inputLayout.visibility = View.GONE
-        addContentView(
+        findViewById<FrameLayout>(android.R.id.content).addView(
             inputLayout,
             FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.MATCH_PARENT,
@@ -47,23 +48,34 @@ class KeyBoardActivity : AppCompatActivity() {
                 fullDisplay = displayHeight
             }
             val keyBoardHeight = screenHeight - displayHeight
-            val scrolly =(keyBoardHeight + inputLayout.height)-( screenHeight - viewbottom)
-
-            Log.d("Keyboard", "Size: ${scrolly}")
+            Log.d("Keyboard", "键盘和虚拟键一起的高度: ${keyBoardHeight}")
             if (displayHeight == fullDisplay) {
                 //说明键盘已经隐藏
-                inputLayout.visibility = View.GONE
-                headview.visibility = View.GONE
-            } else {
-                headview.visibility = View.VISIBLE
-                inputLayout.visibility = View.VISIBLE
-                (inputLayout.layoutParams as FrameLayout.LayoutParams).apply {
-                    bottomMargin = keyBoardHeight-(screenHeight-fullDisplay!!)
+                if(inputLayout.visibility!=View.GONE){
+                    inputLayout.visibility = View.GONE
+                    headview.visibility = View.GONE
                 }
-//                val rect  =  Rect()
-//                inputLayout.getGlobalVisibleRect(rect)
-//                val scrolly =viewbottom-rect.top
-                recycler.scrollBy(0,scrolly)
+            } else {
+                if(inputLayout.visibility!=View.VISIBLE){
+                    inputLayout.visibility = View.VISIBLE
+                    headview.visibility = View.VISIBLE
+                    (inputLayout.layoutParams as FrameLayout.LayoutParams).apply {
+                        bottomMargin = keyBoardHeight-(screenHeight-fullDisplay!!)
+                    }
+
+                    val rect = IntArray(2)
+                    inputLayout.getLocationOnScreen(rect)
+                    Log.d("Keyboard", "输入框到屏幕底部距离${1920-rect[1]}")
+//                val scrolly =(keyBoardHeight + inputLayout.height)-( screenHeight - viewbottom)
+                    val scrolly =(viewbottom-rect[1])
+                    Log.d("Keyboard", "item 应该滚动${scrolly}")
+                                    recycler.smoothScrollBy(0,viewbottom-(displayHeight-inputLayout.height))
+                    if(!inputLayout.findViewById<EditText>(R.id.input).isFocused){
+                        inputLayout.postDelayed({
+                            inputLayout.findViewById<EditText>(R.id.input).requestFocus()
+                        },100)
+                    }
+                }
             }
         })
 //        recycler.setVerticalScrollBarEnabled(false)
@@ -76,9 +88,10 @@ class KeyBoardActivity : AppCompatActivity() {
                 val rect  =  Rect()
                 view.getGlobalVisibleRect(rect)
                 viewbottom = rect.bottom//当前item 到屏幕顶部距离   屏幕高度是1920
+                Log.d("Keyboard", "item 到屏幕底部距离 ${1920-viewbottom}")
                 val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 inputMethodManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
-//                (recycler.layoutManager as LinearLayoutManager).sc
+
             }
         }
     }
